e001cebb1f31674a24b65bcbab6066dca5505ab1
Jabulile <jabulilemsibi184@gmail.com>
2025-08-17 06:50:57 +0200

    Edited index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function showAnswer(response) {
 
 let apiKey = "9c6308te7d4bf50b79ca37c634bbafo5";
 let context = 
-  "be polite and provide a very short answer. make sure to pick one";
-let prompt = "what's the best cuisine in the world?";
+  "be polite and provide a very short answer. Your answer must be straight forward";
+let prompt = "who was the first female president?";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
 
 axios.get(apiUrl).then(showAnswer);
